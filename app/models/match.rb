9098b3174bf067708a795960ce55b6b15fd1d114
Vasilis Spilka <vasspilka@gmail.com>
2016-01-21 20:52:24 +0200

    No need for where

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,22 +3,22 @@ class Match < ActiveRecord::Base
   belongs_to :matched_user, class_name: 'User'
 
   after_create :create_inverse, unless: :has_inverse?
-  after_destroy :destroy_inverses, if: :has_inverse?
+  after_destroy :destroy_inverse, if: :has_inverse?
 
   def create_inverse
     self.class.create(inverse_match_options)
   end
 
-  def destroy_inverses
-    inverses.destroy_all
+  def destroy_inverse
+    inverse.destroy
   end
 
   def has_inverse?
     self.class.exists?(inverse_match_options)
   end
 
-  def inverses
-    self.class.where(inverse_match_options)
+  def inverse
+    self.class.find_by(inverse_match_options)
   end
 
   def inverse_match_options
